df458e6d307ff71a6c914065475509575a6a611e
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-04 22:57:01 -0300

    Prova I 2024.1 Q2.c

--- Prova I 2024.1 Q2.c
+++ Prova I 2024.1 Q2.c
@@ -1,3 +1,16 @@
+/*Um cadeado possui um sistema de código para ser aberto em vez de uma chave. O cadeado 
+contém uma sequência de três rodas. Cada roda possui os 9 dígitos (0..9) em ordem. Se você 
+move uma roda para cima, o dígito que ela mostra muda para o próximo dígito (se o dígito 
+mostrado for '9', então ela muda para '0'). Se você move uma roda para baixo, ela muda para o 
+dígito anterior (se a dígito mostrado for '0', ela muda para '9').
+O cadeado abre quando as rodas mostrarem uma determinada sequência numérica. Escreva um
+programa que leia a configuração atual do cadeado (número atualmente exibido pelas rodas) e o
+número de desbloqueio (a senha armazenada nas rodas) e indique qual o menor número de
+movimentos necessários para abrir o cadeado. Por exemplo, se o número atual for 152 e o
+segredo for 971, serão precisos: dois movimentos para o primeiro número (1→0→9), dois
+movimentos para o segundo número (5→6→7) e um número para o terceiro número (1→2), logo a
+saída deverá ser 5.*/
+
 #include <stdio.h>
 
 main(){
